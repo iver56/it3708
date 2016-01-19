3ffbe22277a3a354b3101b86519ab94343a4e3b7
iver56 <iver56@hotmail.com>
2016-01-19 20:51:17 +0100

    Round obstacles

--- project1/obstacle.py
+++ project1/obstacle.py
@@ -1,9 +1,10 @@
 import gfx
 import object_collection
+import math
 
 
 class Obstacle(object):
-    SIZE = 16
+    SIZE = 8
     COLOR = 255, 255, 255
 
     def __init__(self, x, y):
@@ -11,22 +12,12 @@ class Obstacle(object):
         self.y = y
 
     def draw(self, draw, screen):
-        draw.rect(
-            screen,
-            self.COLOR,
-            [
-                self.x - self.SIZE / 2,
-                self.y - self.SIZE / 2,
-                self.SIZE,
-                self.SIZE
-            ]
-        )
+        draw.circle(screen, self.COLOR, [self.x, self.y], self.SIZE)
 
     @staticmethod
     def obstacle_exists_at_position(x, y):
         for obstacle in object_collection.ObjectCollection.all_obstacles:
-            if obstacle.x - obstacle.SIZE / 2 <= x <= obstacle.x + obstacle.SIZE / 2 and \
-                            obstacle.y - obstacle.SIZE / 2 <= y <= obstacle.y + obstacle.SIZE / 2:
+            if math.sqrt((x - obstacle.x) ** 2 + (y - obstacle.y) ** 2) < Obstacle.SIZE:
                 return True
         return False
 
