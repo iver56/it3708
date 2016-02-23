2d1b6efe7e9594872b5bc036a1dc7d96be1926b3
iver56 <iver56@hotmail.com>
2016-02-23 15:21:13 +0100

    Support random bit pattern in onemax

--- project2/one_max.py
+++ project2/one_max.py
@@ -2,6 +2,7 @@ from individual import Individual
 from genotype import Genotype
 from problem import Problem
 import argparse
+import random
 
 
 class OneMaxGenotype(Genotype):
@@ -9,6 +10,8 @@ class OneMaxGenotype(Genotype):
 
 
 class OneMaxProblem(Problem):
+    TARGET_BIT_PATTERN = None
+
     @staticmethod
     def parse_args():
         arg_parser = argparse.ArgumentParser()
@@ -21,12 +24,32 @@ class OneMaxProblem(Problem):
             required=False,
             default=OneMaxGenotype.GENOTYPE_SIZE
         )
+        arg_parser.add_argument(
+            '--target-bit-pattern',
+            dest='target_bit_pattern',
+            help='This can be set to random in case one does not want the target bit pattern to be all ones',
+            choices=['all_ones', 'random'],
+            type=str,
+            required=False,
+            default='all_ones'
+        )
         args, unknown_args = arg_parser.parse_known_args()
         OneMaxGenotype.GENOTYPE_SIZE = args.genotype_size
+        if args.target_bit_pattern == 'all_ones':
+            OneMaxProblem.TARGET_BIT_PATTERN = [1 for _ in range(OneMaxGenotype.GENOTYPE_SIZE)]  # TODO: [1] * size
+        elif args.target_bit_pattern == 'random':
+            OneMaxProblem.TARGET_BIT_PATTERN = []
+            for i in range(OneMaxGenotype.GENOTYPE_SIZE):
+                OneMaxProblem.TARGET_BIT_PATTERN.append(1 if random.random() > 0.5 else 0)
+            print 'Chosen random target bit pattern:'
+            print OneMaxProblem.TARGET_BIT_PATTERN
 
     @staticmethod
     def calculate_fitness(individual):
-        return sum(individual.phenotype)
+        return sum([
+                       1 if individual.phenotype[i] == OneMaxProblem.TARGET_BIT_PATTERN[i] else 0
+                       for i in range(OneMaxGenotype.GENOTYPE_SIZE)
+                       ])
 
 
 class OneMaxIndividual(Individual):
