f681b666c4596f4166e191305ae020a8b398ca60
iver56 <iver56@hotmail.com>
2016-02-23 13:57:09 +0100

    DRY surprising sequences tests

--- project2/test_surprising_sequences.py
+++ project2/test_surprising_sequences.py
@@ -3,75 +3,39 @@ import surprising_sequences
 
 
 class TestSurprisingSequencesProblem(unittest.TestCase):
-    def test_fitness_global1(self):
-        surprising_sequences.SurprisingSequencesProblem.MODE = 'global'
+    def assert_fitness(self, mode, dna, should_be_surprising):
+        surprising_sequences.SurprisingSequencesProblem.MODE = mode
         alphabet_size = 3
         surprising_sequences.SurprisingSequencesGenotype.set_alphabet(alphabet_size)
         g = surprising_sequences.SurprisingSequencesGenotype(3)
-        g.dna = ['A', 'B', 'C']
+        g.dna = dna
         p = surprising_sequences.SurprisingSequencesIndividual(g)
         fitness = surprising_sequences.SurprisingSequencesProblem.calculate_fitness(p)
-        self.assertAlmostEqual(fitness, 1.0)  # surprising
+        if should_be_surprising:
+            self.assertAlmostEqual(fitness, 1.0)
+        else:
+            self.assertLess(fitness, 1.0)
+
+    def test_fitness_global1(self):
+        self.assert_fitness('global', ['A', 'B', 'C'], should_be_surprising=True)
 
     def test_fitness_global2(self):
-        surprising_sequences.SurprisingSequencesProblem.MODE = 'global'
-        alphabet_size = 3
-        surprising_sequences.SurprisingSequencesGenotype.set_alphabet(alphabet_size)
-        g = surprising_sequences.SurprisingSequencesGenotype(3)
-        g.dna = ['A', 'B', 'C', 'C', 'B', 'A']
-        p = surprising_sequences.SurprisingSequencesIndividual(g)
-        fitness = surprising_sequences.SurprisingSequencesProblem.calculate_fitness(p)
-        self.assertAlmostEqual(fitness, 1.0)  # surprising
+        self.assert_fitness('global', ['A', 'B', 'C', 'C', 'B', 'A'], should_be_surprising=True)
 
     def test_fitness_global3(self):
-        surprising_sequences.SurprisingSequencesProblem.MODE = 'global'
-        alphabet_size = 3
-        surprising_sequences.SurprisingSequencesGenotype.set_alphabet(alphabet_size)
-        g = surprising_sequences.SurprisingSequencesGenotype(3)
-        g.dna = ['A', 'A', 'B', 'C', 'C']
-        p = surprising_sequences.SurprisingSequencesIndividual(g)
-        fitness = surprising_sequences.SurprisingSequencesProblem.calculate_fitness(p)
-        self.assertLess(fitness, 1.0)  # not surprising
+        self.assert_fitness('global', ['A', 'A', 'B', 'C', 'C'], should_be_surprising=False)
 
     def test_fitness_global4(self):
-        surprising_sequences.SurprisingSequencesProblem.MODE = 'global'
-        alphabet_size = 3
-        surprising_sequences.SurprisingSequencesGenotype.set_alphabet(alphabet_size)
-        g = surprising_sequences.SurprisingSequencesGenotype(3)
-        g.dna = ['A', 'B', 'B', 'A', 'C', 'C', 'A']
-        p = surprising_sequences.SurprisingSequencesIndividual(g)
-        fitness = surprising_sequences.SurprisingSequencesProblem.calculate_fitness(p)
-        self.assertLess(fitness, 1.0)  # not surprising
+        self.assert_fitness('global', ['A', 'B', 'B', 'A', 'C', 'C', 'A'], should_be_surprising=False)
 
     def test_fitness_local1(self):
-        surprising_sequences.SurprisingSequencesProblem.MODE = 'local'
-        alphabet_size = 3
-        surprising_sequences.SurprisingSequencesGenotype.set_alphabet(alphabet_size)
-        g = surprising_sequences.SurprisingSequencesGenotype(3)
-        g.dna = ['A', 'A', 'B', 'C', 'C']
-        p = surprising_sequences.SurprisingSequencesIndividual(g)
-        fitness = surprising_sequences.SurprisingSequencesProblem.calculate_fitness(p)
-        self.assertAlmostEqual(fitness, 1.0)  # surprising
+        self.assert_fitness('local', ['A', 'A', 'B', 'C', 'C'], should_be_surprising=True)
 
     def test_fitness_local2(self):
-        surprising_sequences.SurprisingSequencesProblem.MODE = 'local'
-        alphabet_size = 3
-        surprising_sequences.SurprisingSequencesGenotype.set_alphabet(alphabet_size)
-        g = surprising_sequences.SurprisingSequencesGenotype(3)
-        g.dna = ['A', 'B', 'B', 'A', 'C', 'C', 'A']
-        p = surprising_sequences.SurprisingSequencesIndividual(g)
-        fitness = surprising_sequences.SurprisingSequencesProblem.calculate_fitness(p)
-        self.assertAlmostEqual(fitness, 1.0)  # surprising
+        self.assert_fitness('local', ['A', 'B', 'B', 'A', 'C', 'C', 'A'], should_be_surprising=True)
 
     def test_fitness_local3(self):
-        surprising_sequences.SurprisingSequencesProblem.MODE = 'local'
-        alphabet_size = 3
-        surprising_sequences.SurprisingSequencesGenotype.set_alphabet(alphabet_size)
-        g = surprising_sequences.SurprisingSequencesGenotype(3)
-        g.dna = ['A', 'B', 'C', 'B', 'C']
-        p = surprising_sequences.SurprisingSequencesIndividual(g)
-        fitness = surprising_sequences.SurprisingSequencesProblem.calculate_fitness(p)
-        self.assertLess(fitness, 1.0)  # not surprising
+        self.assert_fitness('local', ['A', 'B', 'C', 'B', 'C'], should_be_surprising=False)
 
     def test_phenotype(self):
         g1 = surprising_sequences.SurprisingSequencesGenotype(3)
