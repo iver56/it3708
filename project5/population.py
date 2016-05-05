9a31fca27df65f4fa60cd94dc442793fbaf4706f
iver56 <iver56@hotmail.com>
2016-05-05 14:36:48 +0200

    Implement population.get_non_dominated_individuals

--- project5/population.py
+++ project5/population.py
@@ -5,3 +5,17 @@ class Population(object):
     def set_individuals(self, individuals):
         self.individuals = individuals
 
+    def get_non_dominated_individuals(self):
+        non_dominated_individuals = []
+        for i1 in self.individuals:
+            # check if i1 is dominated by any other individual
+            i1.is_dominated = False
+            for i2 in self.individuals:
+                if i1 == i2:
+                    continue
+                if i2.dominates(i1):
+                    i1.is_dominated = True
+                    break
+            if not i1.is_dominated:
+                non_dominated_individuals.append(i1)
+        return non_dominated_individuals
---
+++ project5/test_population.py
@@ -0,0 +1,24 @@
+import unittest
+from data_manager import dm
+import population
+import genotype
+import individual
+import plot
+
+
+class TestPopulation(unittest.TestCase):
+    def test_non_dominated_individuals(self):
+        # create random population
+        n = 30
+        genotypes = [genotype.Genotype.get_random_genotype() for _ in range(n)]
+        individuals = [individual.Individual(g) for g in genotypes]
+        p = population.Population()
+        p.set_individuals(individuals)
+
+        non_dominated_individuals = p.get_non_dominated_individuals()
+        self.assertGreaterEqual(len(non_dominated_individuals), 1)
+        self.assertLessEqual(len(non_dominated_individuals), len(individuals))
+
+
+if __name__ == '__main__':
+    unittest.main()
