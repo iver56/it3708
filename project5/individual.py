f871de1c4802b2f9e78b609504fc1ad76f64547f
iver56 <iver56@hotmail.com>
2016-05-05 13:44:29 +0200

    Fix bug in tour cost & distance calculation

--- project5/individual.py
+++ project5/individual.py
@@ -7,12 +7,12 @@ class Individual(object):
 
     def calculate_tour_distance(self):
         return sum(
-            dm.get_distance(i, i + 1)
+            dm.get_distance(self.genotype.city_ids[i], self.genotype.city_ids[i + 1])
             for i in range(len(self.genotype.city_ids) - 1)
         )
 
     def calculate_tour_cost(self):
         return sum(
-            dm.get_cost(i, i + 1)
+            dm.get_cost(self.genotype.city_ids[i], self.genotype.city_ids[i + 1])
             for i in range(len(self.genotype.city_ids) - 1)
         )
