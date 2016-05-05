da5bc4b9a67c7ccc8f0781e72f8fda3d387fa737
iver56 <iver56@hotmail.com>
2016-05-05 13:54:06 +0200

    Implement individual.dominates()

--- project5/individual.py
+++ project5/individual.py
@@ -6,13 +6,25 @@ class Individual(object):
         self.genotype = genotype
 
     def calculate_tour_distance(self):
+        # TODO: should be sped up by caching
         return sum(
             dm.get_distance(self.genotype.city_ids[i], self.genotype.city_ids[i + 1])
             for i in range(len(self.genotype.city_ids) - 1)
         )
 
     def calculate_tour_cost(self):
+        # TODO: should be sped up by caching
         return sum(
             dm.get_cost(self.genotype.city_ids[i], self.genotype.city_ids[i + 1])
             for i in range(len(self.genotype.city_ids) - 1)
+        )
+
+    def dominates(self, other_individual):
+        return (
+            self.calculate_tour_distance() <= other_individual.calculate_tour_distance() and  # distance not worse
+            self.calculate_tour_cost() <= other_individual.calculate_tour_cost() and  # cost not worse
+            (
+                self.calculate_tour_distance() < other_individual.calculate_tour_distance() or  # distance is better
+                self.calculate_tour_cost() < other_individual.calculate_tour_cost()  # cost is better
+            )
         )
