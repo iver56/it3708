daca55faf85244d7ce79a9159f7166799cbd8193
iver56 <iver56@hotmail.com>
2016-05-05 13:19:09 +0200

    List comprehensions

--- project5/individual.py
+++ project5/individual.py
@@ -6,13 +6,13 @@ class Individual(object):
         self.genotype = genotype
 
     def calculate_tour_distance(self):
-        distance = 0
-        for i in range(len(self.genotype.city_ids) - 1):
-            distance += dm.get_distance(i, i + 1)
-        return distance
+        return sum(
+            dm.get_distance(i, i + 1)
+            for i in range(len(self.genotype.city_ids) - 1)
+        )
 
     def calculate_tour_cost(self):
-        cost = 0
-        for i in range(len(self.genotype.city_ids) - 1):
-            cost += dm.get_cost(i, i + 1)
-        return cost
+        return sum(
+            dm.get_cost(i, i + 1)
+            for i in range(len(self.genotype.city_ids) - 1)
+        )
