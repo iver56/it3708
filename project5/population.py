7b1b26bba61ea8833f2e3c26da5e9254e7b306f5
iver56 <iver56@hotmail.com>
2016-05-06 14:58:52 +0200

    Optimize for large crowding distance in create_offpring

--- project5/population.py
+++ project5/population.py
@@ -104,5 +104,5 @@ class Population(object):
         if r < self.tournament_selection_epsilon:
             return random.choice(contestants)
         else:
-            sorted_contestants = sorted(contestants, key=lambda p: (p.rank, p.crowding_distance))
+            sorted_contestants = sorted(contestants, key=lambda p: (p.rank, -p.crowding_distance))
             return sorted_contestants[0]
